01551cb872024fb74908456bfc20facadba11fb0
fahadalduaij <fahad_osama@hotmail.com>
2021-09-21 15:55:10 +0300

    Solved the Conditionals task

--- calculator.js
+++ calculator.js
@@ -2,5 +2,31 @@ const prompt = require("prompt-sync")(); // Please don't touch me :)
 
 // The prompt method allows us to take an input from the user.
 // Run the code to see how to use prompt.
-const name = prompt("What's your name? ");
-console.log(`Hi, I'm ${name}.`);
+const firstNumber = prompt("Please enter the first number: ");
+const operation  = prompt("(+, -, /, *): ");
+const secondNumber = prompt("Please enter the second number: ");
+let compute
+
+if ( isNaN(firstNumber) || isNaN(secondNumber) ) {
+    return console.log("The numbers were invalid")
+};
+switch (operation) {
+    case '+':
+        compute = parseInt(firstNumber) + parseInt(secondNumber)
+        break
+    case '-':
+        compute = parseInt(firstNumber) - parseInt(secondNumber)
+        break
+    case '/':
+        compute = parseInt(firstNumber) / parseInt(secondNumber)
+        break
+    case '*':
+        compute = parseInt(firstNumber) * parseInt(secondNumber)
+        break
+    default:
+        return console.log("The operation is not valid")
+}   
+
+
+console.log(`${firstNumber} ${operation} ${secondNumber} = ${compute} `);
+
